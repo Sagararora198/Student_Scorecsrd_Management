3ec0c65624624addbc4693032e957c4559ba6701
Sagararora198 <99186411+Sagararora198@users.noreply.github.com>
2022-04-15 00:19:30 +0530

    first commit

---
+++ add_student.php
@@ -0,0 +1,72 @@
+<?php
+include("report_card.php")
+
+?>
+<style>
+    
+    .primary-btn{
+        
+    display: block;
+    margin: auto;
+    width: 9%;
+    background-color: red;
+    border: 2px solid #eb5a5a;
+    border-radius: 4px;
+    text-align: center;
+    color: white;
+    text-decoration: none;
+
+    }
+</style>    
+<div class="container">
+    <h1>
+        Enter Student Detail Here
+    </h1>
+</div>
+
+<form action="add_student.php" method="post" class="form">
+    <label for="username">Enter full name of the student</label>
+    <input type="text" name="username" id="username" class="username" required >
+
+    <label for="mathnumber">Enter maths number</label>
+    <input type="number" name="mathnumber" id="mathnumber" class="mathnumber" required> 
+
+    <label for="mathnumber">Enter ADSA number </label>
+    <input type="number" name="adsanumber" id="adsanumber" class="adsanumber" required> 
+
+    <label for="mathnumber">Enter DCCN number</label>
+    <input type="number" name="dccnnumber" id="dccnnumber" class="dccnnumber" required> 
+
+    <button type="submit" class="primary_btn ">submit</button>
+</form>
+   
+<div>
+
+    <a href="database.php" class="primary-btn ">Show score card </a>
+</div>
+    
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $servername = 'localhost';
+    $username = 'root';
+    $password = "";
+    $conn = new mysqli($servername,$username,$password);
+    if ($conn->connect_error) {
+        die("Connection failed:" . $conn->connect_error);
+
+    }
+    $name = $_POST['username'];
+    $mathmarks = $_POST['mathnumber'];
+    $adsamarks = $_POST['adsanumber'];
+    $dccnmarks = $_POST['dccnnumber'];
+    $sql = "INSERT INTO `report_card`.`detail`(username,mathmarks,adsamarks,dccnmarks) VALUES('$name','$mathmarks','$adsamarks','$dccnmarks')";
+    if ($conn->query($sql)==TRUE) {
+        echo "<script> alert(`Success`);</script>";
+    }
+    else{
+        echo "<script> alert(`failed`);</script>";
+    }
+    $conn->close();
+}
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include("report_card.php");
+
+?>
+
+<div class="button_area">
+    <a  href="add_student.php" class="primary_btn">Add student score </button>
+    <a  href="database.php" class="primary_btn"> Show report of existing students</button>
+</div>
